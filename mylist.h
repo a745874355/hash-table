5e4fd18f583c1e048ff3697801802cc15aa15083
赵晨 <zhaochen@zhaochendeMacBook-Pro.local>
2020-03-02 18:02:36 -0500

    update mylist.h and table.h

--- mylist.h
+++ mylist.h
@@ -1,3 +1,261 @@
 /**************************************************/
 /*  Put your linked list code into this file      */
 /**************************************************/
+/**************************************************/
+/*                                                */
+/*  Lab 4 linkedlist file                         */
+/**************************************************/
+
+#include <string>
+#include <iostream>
+
+template <typename T>
+class DList{
+    
+    struct Node{
+        T data_;
+        Node* next_;
+        Node* prev_;
+        Node(const T& data=T{},Node* next=nullptr, Node* prev=nullptr){
+            data_=data;
+            next_=next;
+            prev_=prev;
+        }
+    };
+    Node* front_;
+    Node* back_;
+    
+public:
+    DList(){
+        front_=nullptr;
+        back_=nullptr;
+    }
+    void push_front(const T& data);
+    void push_back(const T& data);
+    void pop_front();
+    void pop_back();
+    void print() const;
+    void reversePrint() const;
+    ~DList();
+    
+    class const_iterator{
+        template <typename> friend class DList;
+        const DList* myList_;
+        Node* curr_;
+        const_iterator(Node* curr, const DList* theList){
+            curr_ = curr;
+            myList_=theList;
+        }
+    public:
+        const_iterator(){
+            myList_=nullptr;
+            curr_=nullptr;
+        }
+        //prefix
+        const_iterator operator++(){
+            //        if(curr_ != myList_->back_)
+            curr_ = curr_->next_;
+            return *this;
+        }
+        //postfix
+        const_iterator operator++(int){
+            const_iterator old(*this);
+            if (curr_ != myList_->back_->next_)
+                curr_ = curr_->next_;
+            return  old;
+        }
+        //prefix
+        const_iterator operator--(){
+            if(curr_){
+                curr_=curr_->prev_;
+            }
+            else{
+                if(myList_){
+                    curr_=myList_->back_;
+                }
+            }
+            return *this;
+        }
+        //postfix
+        const_iterator operator--(int){
+            const_iterator old(*this);
+            if(curr_){
+                curr_=curr_->prev_;
+            }
+            else{
+                if(myList_){
+                    curr_=myList_->back_;
+                }
+            }
+            return  old;
+        }
+        const T& operator*() const{return curr_->data_;}
+        bool operator==(const_iterator rhs) const{
+            bool rc=false;
+            if(myList_==rhs.myList_ && curr_==rhs.curr_)
+                rc=true;
+            return rc;
+        }
+        bool operator!=(const_iterator rhs) const{
+            return !(*this==rhs);
+        }
+    };
+    class iterator:public const_iterator{
+        template <typename> friend class DList;
+    protected:
+        iterator(Node* curr,DList* theList):const_iterator(curr,theList){}
+    public:
+        iterator():const_iterator(){}
+        iterator operator++(){
+            if (this->curr_ != this->myList_->back_->next_)
+                (const_iterator)(*this)++;
+            return *this;
+        }
+        iterator operator++(int){
+            iterator rc(*this);
+            if (this->curr_ != this->myList_->back_->next_)
+                const_iterator::operator++();
+            return rc;
+        }
+        iterator operator--(){
+            --(*this);
+            return *this;
+        }
+        iterator operator--(int){
+            iterator rc (*this);
+            --(*this);
+            return rc;
+        }
+        T& operator*(){
+            return this->curr_->data_;
+        }
+        const T& operator*()const{
+            return this->curr_->data_;
+        }
+    };
+    
+    DList::const_iterator cbegin() const{
+        if(front_ != nullptr)
+            return DList::const_iterator(front_, this);
+        else
+            return DList::const_iterator(nullptr, this);
+    }
+    DList::iterator begin(){
+        if (front_ != nullptr)
+            return DList::iterator(front_, this);
+        
+        else
+            return DList::iterator(nullptr, this);
+    }
+    DList::const_iterator cend() const{
+        return DList::const_iterator(nullptr,this);
+    }
+    
+    DList::iterator end(){
+        if(front_ == nullptr)
+            return DList::iterator(front_,this);
+        else
+            return DList::iterator(back_->next_,this);
+    }
+};
+
+//template <typename T>
+//DList<T>::~DList(){
+//    Node* curr = front_;
+//    while (curr!= back_){
+//        curr = curr->next_;
+//        delete curr->prev_;
+//    }
+//    if(back_!= nullptr){
+//        delete back_;
+//    }
+//}
+template <typename T>
+DList<T>::~DList(){
+    Node* curr = front_;
+    
+    while (curr!= nullptr){
+        Node* rm = curr;
+        curr = curr->next_;
+        delete rm;
+    }
+}
+template <typename T>
+void DList<T>::push_front(const T& data){
+    
+    Node* nn=new Node(data,front_);
+    if(front_!=nullptr){
+        front_->prev_=nn;
+    }
+    else{
+        back_ = nn;
+    }
+    front_ = nn;
+}
+
+template <typename T>
+void DList<T>::push_back(const T& data){
+    Node* nn = new Node(data, nullptr,back_);
+    if(back_!= nullptr){
+        back_->next_ = nn;
+    }else{
+        front_ = nn;
+    }
+    back_ = nn;
+}
+
+template <typename T>
+void DList<T>::pop_front(){
+    if(front_!=nullptr){
+        Node* rm = front_;
+        if(front_->next_!=nullptr){
+            front_=front_->next_;
+            front_->prev_=nullptr;
+        }
+        else{
+            front_=nullptr;
+            back_=nullptr;
+        }
+        
+        delete rm;
+    }
+}
+template <typename T>
+void DList<T>::pop_back(){
+    if(back_!=nullptr){
+        Node* rm = back_;
+        if(back_->prev_!=nullptr){
+            back_=back_->prev_;
+            back_->next_=nullptr;
+        }
+        else{
+            front_=nullptr;
+            back_=nullptr;
+        }
+        delete rm;
+    }
+}
+template <typename T>
+void DList<T>::print() const{
+    Node* curr=front_;
+    while(curr!=nullptr){
+        std::cout << curr->data_ << " ";
+        curr=curr->next_;
+    }
+    if(!front_){
+        std::cout << "empty list";
+    }
+    std::cout << std::endl;
+}
+template <typename T>
+void DList<T>::reversePrint() const{
+    Node* curr=back_;
+    while(curr!=nullptr){
+        std::cout << curr->data_ << " ";
+        curr=curr->prev_;
+    }
+    if(!back_){
+        std::cout << "empty list";
+    }
+    std::cout << std::endl;
+}
--- table.h
+++ table.h
@@ -9,7 +9,8 @@
 #include <utility>
 #include <functional>
 #include "mylist.h"
-
+//Table is the abstract base class, describes the functianalities of the Table class
+using std::string;
 template <class TYPE>
 class Table{
 public:
@@ -22,6 +23,7 @@ public:
 	virtual ~Table(){}
 };
 
+//SimpleTable implements the abstract table class
 template <class TYPE>
 class SimpleTable:public Table<TYPE>{
 
@@ -39,17 +41,18 @@ class SimpleTable:public Table<TYPE>{
 	int capacity_;       //capacity of the array
 
 	void grow(){
-		Record** tmp=new Record*[capacity_+10];
-		for(int i=0;i<capacity_;i++){
-			tmp[i]=records_[i];
+		Record** tmp=new Record*[capacity_+10];                          //1+1 ------------------------------>need update because this is not efficient in grow capacity.
+		for(int i=0;i<capacity_;i++){                                    //1 + capacity + 1
+			tmp[i]=records_[i];                                          //capacity
 		}
-		for(int i=capacity_;i <capacity_+10;i++){
-			tmp[i]=nullptr;
+		for(int i=capacity_;i <capacity_+10;i++){                        //10
+			tmp[i]=nullptr;                                              //10
 		}
-		delete[] records_;
-		records_=tmp;
-		capacity_+=10;
-	}
+		delete[] records_;                                               //1
+		records_=tmp;                                                    //1
+		capacity_+=10;                                                   //1
+	}                                                                    //26+2*capacity
+                                                                         //because capacity is consider as the n,so run time is O(n)
 public:
 	SimpleTable(int capacity);
 	SimpleTable(const SimpleTable& other);
@@ -66,143 +69,150 @@ public:
 
 template <class TYPE>
 int SimpleTable<TYPE>::numRecords() const{
-	int rc=0;
-	for(int i=0;records_[i]!=nullptr;i++){
-		rc++;
-	}
-	return rc;
+    int rc=0;                                                                   //1                  -------------------------> can change to:
+	for(int i=0;records_[i]!=nullptr;i++){                                      //1+capacity+capacity                          for(int i=0;records_[i]!=nullptr&&i<capacity;i++)"
+		rc++;                                                                   //capacity
+	}                                                                           //2+3*capacity
+	return rc;                                                                  //runtime is O(n)
 }
 
 
 
 template <class TYPE>
-SimpleTable<TYPE>::SimpleTable(int capacity): Table<TYPE>(){
-	records_=new Record*[capacity];
-	capacity_=capacity;
-	for(int i=0;i<capacity_;i++){
-		records_[i]=nullptr;
-	}
+SimpleTable<TYPE>::SimpleTable(int capacity): Table<TYPE>(){                    //
+	records_=new Record*[capacity];                                             //1
+	capacity_=capacity;                                                         //1
+	for(int i=0;i<capacity_;i++){                                               //1+capacity+capacity
+		records_[i]=nullptr;                                                    //capacity
+	}                                                                           //3+3*capacity
+                                                                                //runtime is O(n)
 }
 
 template <class TYPE>
 SimpleTable<TYPE>::SimpleTable(const SimpleTable<TYPE>& other){
-	records_=new Record*[other.capacity_];
-	capacity_=other.capacity_;
-	for(int i=0;i<other.numRecords();i++){
-		update(other.records_[i]->key_,other.records_[i]->data_);
-	}
-}
-template <class TYPE>
-SimpleTable<TYPE>::SimpleTable(SimpleTable<TYPE>&& other){
-	capacity_=other.capacity_;
-	records_=other.records_;
-	other.records_=nullptr;
-	other.capacity_=0;
+	records_=new Record*[other.capacity_];                                      //1
+	capacity_=other.capacity_;                                                  //1
+	for(int i=0;i<other.numRecords();i++){                                      //1+capacity+capacity
+		update(other.records_[i]->key_,other.records_[i]->data_);               //O(n!)
+	}                                                                           //thus overall runtime is O(n!)
 }
 
 template <class TYPE>
-void SimpleTable<TYPE>::update(const string& key, const TYPE& value){
-	int idx=-1;
-	int size=numRecords();
-	for(int i=0;i<size;i++){
-		if(records_[i]->key_ == key){
-			idx=i;
+SimpleTable<TYPE>::SimpleTable(SimpleTable<TYPE>&& other){
+	capacity_=other.capacity_;                                                  //1
+	records_=other.records_;                                                    //1
+	other.records_=nullptr;                                                     //1
+	other.capacity_=0;                                                          //1
+}                                                                               //runtime is O(1)
+
+template <class TYPE>
+void SimpleTable<TYPE>::update(const string& key, const TYPE& value){           //
+	int idx=-1;                                                                 //1
+	int size=numRecords();                                                      //1+O(n)
+	for(int i=0;i<size;i++){                                                    //1+capacity +capacity
+		if(records_[i]->key_ == key){                                           //capacity
+			idx=i;                                                              //capacity
 		}
 	}
-	if(idx==-1){
-		if(size == capacity_){
-			grow();
+	if(idx==-1){                                                                //1
+		if(size == capacity_){                                                  //1
+			grow();                                                             //O(1)
 		}
-		records_[size++]=new Record(key,value);
-		for(int i=0;i<size-1;i++){
-			for(int j=0;j<size-1-i;j++){
-				if(records_[j]->key_ > records_[j+1]->key_){
-					Record* tmp=records_[j];
-					records_[j]=records_[j+1];
-					records_[j+1]=tmp;
+		records_[size++]=new Record(key,value);                                 //1
+                                                                                //----->bubble sort is slow, could be improved by changing to Insertion sort，becuuse the datasturcture
+                                                                                //is sortted array, so do the sort insertion could be much faster than the other sort methord
+		for(int i=0;i<size-1;i++){                                              //1+(capacity-1) +(capacity-1)
+			for(int j=0;j<size-1-i;j++){                                        //1*(capacity-1)+(capacity-1)! +(capacity-1)!
+				if(records_[j]->key_ > records_[j+1]->key_){                    //(capacity-1)!
+					Record* tmp=records_[j];                                    //(capacity-1)!
+					records_[j]=records_[j+1];                                  //(capacity-1)!
+					records_[j+1]=tmp;                                          //(capacity-1)!
 				}
 			}
 		}
 	}
 	else{
 		records_[idx]->data_=value;
-	}
-
+	}                                                                           //because (capacity-1)! is O(n!)
+                                                                                //thus overall runtime is O(n)
 }
 
 template <class TYPE>
 bool SimpleTable<TYPE>::remove(const string& key){
-	int idx=-1;
-	for(int i=0;i<numRecords();i++){
-		if(records_[i]->key_ == key){
-			idx=i;
+	int idx=-1;                                                                 //1
+	for(int i=0;i<numRecords();i++){                                            //1+(1+O(n))*capacity + capacity = 1 + capacity^2 + 2*capacity
+                                                                                // ------------->need fix, put numRecords() out of the for loop, then the runtime could be O(n)
+		if(records_[i]->key_ == key){                                           //capacity
+			idx=i;                                                              //capacity
 		}
 	}
-	if(idx!=-1){
-		delete records_[idx];
-		int size=numRecords();
-		for(int i=idx;i<size-1;i++){
-			records_[i]=records_[i+1];
+	if(idx!=-1){                                                                //1
+		delete records_[idx];                                                   //1
+		int size=numRecords();                                                  //1+O(n)
+		for(int i=idx;i<size-1;i++){                                            //1+(capacity-1) +(capacity-1)
+			records_[i]=records_[i+1];                                          //(capacity-1) +(capacity-1)
 		}
-		records_[size-1]=nullptr;
-		return true;
+		records_[size-1]=nullptr;                                               //1
+		return true;                                                            //1
 	}
 	else{
 		return false;
-	}
+	}                                                                           //thus overall runtime is O(n^2)
 }
 
 template <class TYPE>
 bool SimpleTable<TYPE>::find(const string& key, TYPE& value){
-	int idx=-1;
-	for(int i=0;i<numRecords();i++){
-		if(records_[i]->key_ == key){
-			idx=i;
+	int idx=-1;                                                                 //1
+	for(int i=0;i<numRecords();i++){                                            //1 + (1+O(n))*capacity + capacity = 1 + capacity^2 + 2*capacity
+                                                                                // ------------->need fix, put numRecords() out of the for loop,then the runtime could be O(n)
+		if(records_[i]->key_ == key){                                           //capacity
+			idx=i;                                                              //capacity
 		}
 	}
-	if(idx==-1)
-		return false;
+	if(idx==-1)                                                                 //1
+		return false;                                                           //1
 	else{
-		value=records_[idx]->data_;
-		return true;
+		value=records_[idx]->data_;                                             //1
+		return true;                                                            //1
 	}
-}
+}                                                                               //thus overall runtime is O(n^2)
 
 template <class TYPE>
 const SimpleTable<TYPE>& SimpleTable<TYPE>::operator=(const SimpleTable<TYPE>& other){
-	if(this!=&other){
-		if(records_){
-			int sz=numRecords();
-			for(int i=0;i<sz;i++){
-				remove(records_[0]->key_);
+	if(this!=&other){                                                           //1
+		if(records_){                                                           //1
+			int sz=numRecords();                                                //1+ O(n)
+			for(int i=0;i<sz;i++){                                              //1+capacity + capacity
+				remove(records_[0]->key_);                                      //capacity
 			}
-			delete [] records_;
+			delete [] records_;                                                 //1
 		}
-		records_=new Record*[other.capacity_];
-		capacity_=other.capacity_;
-		int size=other.numRecords();
-		for(int i=0;i<size;i++){
-			update(other.records_[i]->key_,other.records_[i]->data_);
+		records_=new Record*[other.capacity_];                                  //1
+		capacity_=other.capacity_;                                              //1
+		int size=other.numRecords();                                            //1
+		for(int i=0;i<size;i++){                                                //1+capacity + capacity
+			update(other.records_[i]->key_,other.records_[i]->data_);           //capacity
 		}
 
 	}
-	return *this;
+	return *this;                                                               //1
+                                                                                //thus overall runtime is O(n)
 }
 template <class TYPE>
 const SimpleTable<TYPE>& SimpleTable<TYPE>::operator=(SimpleTable<TYPE>&& other){
-	swap(records_,other.records_);
-	swap(capacity_,other.capacity_);
-	return *this;
-}
+	swap(records_,other.records_);                                              //1
+	swap(capacity_,other.capacity_);                                            //1
+	return *this;                                                               //1
+}                                                                               //runtime is O(1)
 template <class TYPE>
 SimpleTable<TYPE>::~SimpleTable(){
-	if(records_){
-		int sz=numRecords();
-		for(int i=0;i<sz;i++){
-			remove(records_[0]->key_);
+	if(records_){                                                               //1
+		int sz=numRecords();                                                    //1 + O（n）
+		for(int i=0;i<sz;i++){                                                  //1 + capacity + capacity
+			remove(records_[0]->key_);                                          //capacity
 		}
-		delete [] records_;
-	}
+		delete [] records_;                                                     //1
+	}                                                                           //thus overall runtime is O(n)
 }
 
 template <class TYPE>
